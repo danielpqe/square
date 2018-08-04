19881639f338cad64604b1b82360ad9ce3cba4aa
danielpqe <danielpqe@gmail.com>
2018-08-03 23:37:54 -0500

    before all data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'square';
+  title = 'Square';
+  a = 3;
+  b = 5;
+  listo=false;
 }
